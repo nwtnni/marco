1eb6eb93471d76ab49e43c7a8108c00a4c117659
Newton Ni <nwtnni@gmail.com>
2020-01-20 23:20:20 -0500

    Obtain public IP address

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::anyhow;
+use anyhow::Context;
+
+static CLOUDFLARE_API_TOKEN: &str = env!("CLOUDFLARE_API_TOKEN");
+
+fn main() -> anyhow::Result<()> {
+
+    let ip = ureq::get("http://icanhazip.com")
+        .timeout_connect(1_000)
+        .call()
+        .into_string()
+        .with_context(|| anyhow!("Could not parse response as UTF-8"))?
+        .trim()
+        .to_owned();
+
+    println!("{}", ip);
+
+    Ok(())
+
 }
