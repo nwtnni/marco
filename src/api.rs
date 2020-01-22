a3c677e902c17791f0fd7ed9167e63278a8037a4
Newton Ni <nwtnni@gmail.com>
2020-01-21 20:01:41 -0500

    Implement other API calls

--- src/api.rs
+++ src/api.rs
@@ -1,8 +1,12 @@
 use std::any;
+use std::error;
+use std::fmt;
+use std::net;
 
 use anyhow::anyhow;
 use anyhow::Context;
 use serde::Deserialize;
+use serde::Serialize;
 
 static CLOUDFLARE_API_URL: &str = "https://api.cloudflare.com/client/v4/";
 
@@ -16,14 +20,91 @@ pub struct Client<'c> {
 /// https://api.cloudflare.com/#zone-list-zones
 #[derive(Debug, Deserialize)]
 struct Zones {
+    #[serde(flatten)]
+    meta: Meta,
     result: Vec<Zone>,
+}
+
+impl From<Zones> for anyhow::Result<Vec<Zone>> {
+    fn from(zones: Zones) -> anyhow::Result<Vec<Zone>> {
+        anyhow::Result::<()>::from(zones.meta)?;
+        Ok(zones.result)
+    }
 }
 
 /// Represents a single zone from `GET /zones`.
 #[derive(Debug, Deserialize)]
 struct Zone {
-    id: String,
+    id: ZoneID,
     name: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ZoneID(String);
+
+impl fmt::Display for ZoneID {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        write!(fmt, "{}", self.0)
+    }
+}
+
+#[derive(Debug, Deserialize)]
+struct Records {
+    #[serde(flatten)]
+    meta: Meta,
+    result: Vec<Record>, 
+}
+
+impl From<Records> for anyhow::Result<Vec<Record>> {
+    fn from(records: Records) -> anyhow::Result<Vec<Record>> {
+        anyhow::Result::<()>::from(records.meta)?;
+        Ok(records.result)
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Record {
+    id: RecordID,
+    r#type: String,
+    name: String,
+    content: net::IpAddr,
+    ttl: serde_json::Value,
+    proxied: bool,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct RecordID(String);
+
+impl fmt::Display for RecordID {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        write!(fmt, "{}", self.0)
+    }
+}
+
+#[derive(Debug, Deserialize)]
+struct Meta {
+    success: bool,
+    errors: serde_json::Value,
+    messages: serde_json::Value,
+}
+
+impl fmt::Display for Meta {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        writeln!(fmt, "{:#}", self.errors)?;
+        write!(fmt, "{:#}", self.messages)
+    }
+}
+
+impl error::Error for Meta {}
+
+impl From<Meta> for anyhow::Result<()> {
+    fn from(meta: Meta) -> anyhow::Result<()> {
+        if meta.success {
+            Ok(())
+        } else {
+            Err(anyhow!(meta))
+        }
+    }
 }
 
 impl<'c> Client<'c> {
@@ -35,19 +116,51 @@ impl<'c> Client<'c> {
     }
 
     /// Retrieve the ID of a single zone.
-    pub fn get_zone_id(&self, name: &str) -> anyhow::Result<String> {
+    pub fn get_zone_id(&self, zone_name: &str) -> anyhow::Result<ZoneID> {
         let mut zones = self
             .get::<Zones>("zones")
-            .with_context(|| format!("Could not get ID of zone {}", name))?
-            .result;
+            .with_context(|| format!("Could not get ID of zone {}", zone_name))
+            .and_then(anyhow::Result::<Vec<Zone>>::from)?;
 
-        zones.retain(|zone| &zone.name == name);
+        zones.retain(|zone| &zone.name == zone_name);
 
         match zones.len() {
         | 0 => Err(anyhow!("No matching zones found")),
         | 1 => Ok(zones.remove(0).id),
         | n => Err(anyhow!("{} matching zones found, could not select one", n)),
         }
+    }
+
+    pub fn get_dns_record(
+        &self,
+        zone_name: &str,
+        zone_id: &ZoneID,
+        record_name: &str,
+    ) -> anyhow::Result<Record> {
+        let mut records = self
+            .get::<Records>(&format!("zones/{}/dns_records", zone_id))
+            .with_context(|| format!("Could not get DNS record for {} in zone {}", record_name, zone_name))
+            .and_then(anyhow::Result::<Vec<Record>>::from)?;
+
+        records.retain(|record| &record.name == record_name);
+
+        match records.len() {
+        | 0 => Err(anyhow!("No matching DNS records found")),
+        | 1 => Ok(records.remove(0)),
+        | n => Err(anyhow!("{} matching records found, could not select one", n)),
+        }
+    }
+
+    pub fn put_dns_record(
+        &self,
+        zone_id: &ZoneID,
+        zone_name: &str,
+        record_id: &RecordID,
+        record_name: &str,
+        record: &Record,
+    ) -> anyhow::Result<()> {
+        self.put(&format!("zones/{}/dns_records/{}", zone_id, record_id), record)
+            .with_context(|| format!("Could not PUT DNS record for {} in zone {}", record_name, zone_name))
     }
 
     fn get<T: serde::de::DeserializeOwned>(&self, route: &str) -> anyhow::Result<T> {
